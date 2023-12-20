944a915f7e5c6fda657476bdfdf3a536e53eed2f
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-20 15:13:22 +0200

    11.4 validate course name

--- controller/courseController.js
+++ controller/courseController.js
@@ -1,10 +1,23 @@
 import * as courseService from "../service/courseService.js";
 import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` })
 
+const validator = z.object({
+    name: z.string().min(4, { message: "The course name should be a string of at least 4 characters." })
+})
+
 const addCourse = async(c) => {
     const body = await c.req.parseBody()
+    const validationResult = validator.safeParse(body)
+    if(!validationResult.success){
+        return c.html(eta.render("courses.eta", {
+            ...body,
+            errors: validationResult.error.format(),
+            courses: await courseService.listCourses()
+        }))
+    }
     await courseService.addCourse(body)
 
     return c.redirect("/courses")
